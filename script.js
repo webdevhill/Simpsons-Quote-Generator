524b3d7398f27c432d2b1bf56864e9799db14aca
Jeff Hill <jdhill2142@gmail.com>
2023-08-18 14:48:43 -0400

    file and loader updates

--- script.js
+++ script.js
@@ -1,94 +1,61 @@
+const loader = document.getElementById('loader');
 const quoteContainer = document.getElementById('quote-container');
 const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const simpsonsQuoteBtn = document.getElementById('simpson-quote');
 const imgSimpsonCharacter = document.getElementById('simpson-character');
+const simpsonsQuoteUrl = 'https://thesimpsonsquoteapi.glitch.me/quotes';
 let quote = "";
 let author = "";
+let errorTimeOut = 0;
 
-//custom server to avoid  CORS issue
-const proxyUrl = 'https://dry-cove-58467.herokuapp.com/';
-const apiUrl =
-    'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
-const simpsonsQuoteUrl = 'https://thesimpsonsquoteapi.glitch.me/quotes';
-
-
-const loader = document.getElementById('loader');
-var errorTimeOut = 0;
-
-// Functions to display or hide loader and image
-function showLoadingSpinner() {
+// Show Loading
+function loading()  {
     loader.hidden = false;
     quoteContainer.hidden = true;
 }
 
-function removeLoadingSpinner() {
-    if (!loader.hidden) {
-        quoteContainer.hidden = false;
-        loader.hidden = true;
-    } 
+// Hide Loading
+function complete() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
 }
 
-function showCharacterImage(imageUrl) {
-    if (imageUrl) {
-        imgSimpsonCharacter.hidden = false;
-        imgSimpsonCharacter.src = imageUrl;
-    } else {
-        hideCharacterImage();
-    }
-}
-
-function hideCharacterImage() {
-    imgSimpsonCharacter.hidden = true;
-}
-
-//Dynamically Reduce quote font size for long quote
-const maxQuoteLength = 80;
-if (quote.length > maxQuoteLength) {
-    quoteText.classList.add('long-quote');
-} else {
-    quoteText.classList.remove('long-quote');
-}
-
-
+// Get Quote 
 async function getQuote(quoteType) {
-
-    showLoadingSpinner();
-    hideCharacterImage();
-
+    loading();
     try {
-        let url = proxyUrl + simpsonsQuoteUrl;
-
+        let url = simpsonsQuoteUrl;
         const response = await fetch(url);
         const data = await response.json();
-        console.log(data);
-
         quote = data[0].quote;
         author = data[0].character;
-        showCharacterImage(data[0].image);
-
+        imgSimpsonCharacter.src = (data[0].image);
         authorText.innerText = author;
-
         quoteText.innerText = quote;
 
-        removeLoadingSpinner();
+        //Dynamically Reduce quote font size for long quote
+        const maxQuoteLength = 80;
+        if (quote.length > maxQuoteLength) {
+            quoteText.classList.add('long-quote');
+        } else {
+            quoteText.classList.remove('long-quote');
+        }
 
     } catch (error) {
-        //counter to avoid infinite recursive function call thresh hold set to 30
+        //counter to avoid infinite recursive function call - threshhold set to 50
         const errorThreshhold = 50;
         if (errorTimeOut <= errorThreshhold) {
             getQuote();
-
         } else {
             quoteText.innerText = "Oops...Sorry. There was an error. Please try again later."
         }
         errorTimeOut += 1;
-
         console.log(" Error fetching quote: " + error);
     }
+    complete();
 }
-
 
 function tweetQuote() {
     const quote = quoteText.innerText;
